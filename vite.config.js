01b847644203759f973d034c5063e5d113d3c35a
amaya <amaya.gaikwad@healthfirsttech.com>
2026-07-09 16:34:05 +0530

    pwa

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       devOptions: {
         enabled: true
       },
-      includeAssets: ['favicon.svg', 'icon-192.png', 'icon-512.png'],
+      includeAssets: ['favicon.svg', 'icon-192.png', '192.png', 'icon-512.png', '512.png'],
       manifest: {
         name: 'AMAYA',
         short_name: 'AMAYA',
@@ -29,7 +29,7 @@ export default defineConfig({
             purpose: 'any'
           },
           {
-            src: '/icon-192.png',
+            src: '/192.png',
             sizes: '192x192',
             type: 'image/png',
             purpose: 'maskable'
@@ -41,7 +41,7 @@ export default defineConfig({
             purpose: 'any'
           },
           {
-            src: '/icon-512.png',
+            src: '/512.png',
             sizes: '512x512',
             type: 'image/png',
             purpose: 'maskable'
